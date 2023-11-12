6aa4a8051b0de8d198a939be21e959232594888b
RamzyAR7 <rameya683@gmail.com>
2023-11-12 07:46:03 +0200

    fix4

--- printf.c
+++ printf.c
@@ -24,7 +24,7 @@ int _printf(const char *format, ...)
 
 	};
 
-	if (!format)
+	if (!format || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	va_start(args, format);
 	result = copy((char *)format, result);
