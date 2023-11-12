5fdd4609de5449e06cdf400fe3ccf00b03a30ea5
RamzyAR7 <rameya683@gmail.com>
2023-11-12 22:04:54 +0200

    add X,x

--- check_sp2.c
+++ check_sp2.c
@@ -50,3 +50,73 @@ char *intToBin(unsigned int num)
 	bNum[j] = '\0';
 	return (bNum);
 }
+/**
+ * print_hexa_upper - print int as a hex
+ * @args: arguments list
+ *
+ * Return: pointer to the hex value
+ */
+char *print_hexa_upper(va_list args)
+{
+	char *p = intToHexa(va_arg(args, int), 'X');
+
+	return (p);
+}
+/**
+ * print_hexa_lower - print int as a hex
+ * @args: arguments list
+ *
+ * Return: pointer to the hex value
+ */
+char *print_hexa_lower(va_list args)
+{
+	char *p = intToHexa(va_arg(args, int), 'x');
+
+	return (p);
+}
+/**
+ * intToHexa - convert int as a hex
+ * @num: the integar number
+ * @format: the format specifier
+ * Return: pointer to the hex value
+ */
+char *intToHexa(unsigned int num, char format)
+{
+	char *bNum = malloc(32);
+	char temp[32];
+	int i = 0, j = 0;
+
+	if (!bNum)
+	{
+		free(bNum);
+		exit(1);
+		return (NULL);
+	}
+
+	if (num == 0)
+	{
+		bNum[0] = '0';
+		bNum[1] = '\0';
+		return (bNum);
+	}
+
+	while (num > 0)
+	{
+		int remainder = num % 16;
+
+		if (remainder < 10)
+			temp[i++] = remainder + '0';
+		else
+			temp[i++] = remainder - 10 + (format == 'X' ? 'A' : 'a');
+		num /= 16;
+	}
+
+	while (j < i)
+	{
+		bNum[j] = temp[i - j - 1];
+		j++;
+	}
+	bNum[j] = '\0';
+
+	return (bNum);
+}
--- main.h
+++ main.h
@@ -30,6 +30,9 @@ char *print_character(va_list args);
 char *print_integer(va_list args);
 char *print_binary(va_list args);
 char *print_rec(int num, char *p);
+char *print_hexa_upper(va_list args);
+char *print_hexa_lower(va_list args);
+char *intToHexa(unsigned int num, char format);
 char *get_value(char *str, types t[], va_list args, int *nul_c);
 char *intToBin(unsigned int num);
 #endif
