bab74f98007e36f82a3fe15c2790040ab3db052b
RamzyAR7 <rameya683@gmail.com>
2023-11-14 11:08:44 +0200

    fix

--- printf.c
+++ printf.c
@@ -36,7 +36,10 @@ int _printf(const char *format, ...)
 	free(t);
 	return (len_res);
 }
-
+/**
+ * sp_struct - creates an array of structs
+ * Return: pointer to the array
+ */
 types *sp_struct()
 {
 	types *t = (types *)malloc(sizeof(types) * 14);
@@ -65,35 +68,4 @@ types *sp_struct()
 	}
 	return (t);
 }
-/**
- * check_valid_format - check if the input is valid
- * @format: format specifiers
- * Return: 1 if valid and 0 if not
- */
-int check_valid_format(const char *format)
-{
-	int i = 0;
-	int percent = 0;
-	int letters = 0;
 
-	while (format[i])
-	{
-		if (format[i] == '%')
-		{
-			percent++;
-		}
-		else if (format[i] != ' ')
-		{
-			letters++;
-		}
-		i++;
-	}
-	if (percent > 1 || (letters && percent) || percent != 1)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-}
