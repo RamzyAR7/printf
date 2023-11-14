5978b3a59e77f4e31e9c1808653d505d5a63cfe3
RamzyAR7 <rameya683@gmail.com>
2023-11-14 04:28:43 +0200

    add %R

--- check_str_rev.c
+++ check_str_rev.c
@@ -39,3 +39,52 @@ char *print_str_rev(va_list args, char *flags)
 	}
 	return (p);
 }
+/**
+ * print_rot13 - prints a string in rot13
+ * @args: arguments list
+ * @flags: string of flags
+ * Return: pointer to string
+ */
+char *print_rot13(va_list args, char *flags)
+{
+	char *str = va_arg(args, char *);
+	char *p = NULL;
+	int i = 0, j = 0, k = 0, l = 0;
+	char *alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *rot13 = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	if (!str)
+		return (NULL);
+
+	while (str[l])
+	{
+		l++;
+	}
+	p = (char *)malloc(l + 1);
+	if (!p)
+	{
+		free(p);
+		exit(1);
+		return (NULL);
+	}
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; alpha[j]; j++)
+		{
+			if (str[i] == alpha[j])
+			{
+				p[k] = rot13[j];
+				break;
+			}
+		}
+		if (!alpha[j])
+			p[k] = str[i];
+		k++;
+	}
+	p[k] = '\0';
+	if (*flags)
+	{
+		p = choose_flag(p, flags, 'R');
+	}
+	return (p);
+}
