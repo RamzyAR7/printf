7e4160c19cc706296af0fc1ef52e1d6ea8ef4859
Ashraf Atef <ashraf.programer@gmail.com>
2023-11-14 22:51:01 +0200

    task

--- check_flg.c
+++ check_flg.c
@@ -44,8 +44,10 @@ char *choose_flag(char *str, char *flags, char sp_letter)
 		default:
 			if (flags[i] >= '0' && flags[i] <= '9' &&
 				(sp_letter == 'd' || sp_letter == 'i'))
+			{
 				p = flag_width(p, flags + i);
-				break;
+			}
+			break;
 		}
 	}
 	return (p);
